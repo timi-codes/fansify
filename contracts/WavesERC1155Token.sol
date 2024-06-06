7bca67974d6dd787e807e0364009875ac8fb8431
--replace <tejumoladavid@gmail.com>
2024-06-05 22:46:17 -0400

    feat(contracts): implement smart contracts

--- contracts/WavesERC1155Token.sol
+++ contracts/WavesERC1155Token.sol
@@ -9,17 +9,50 @@ pragma solidity ^0.8.24;
         - Should be ERC1155
 */
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract WavesERC1155Token is ERC1155, Ownable {
-    constructor(string memory uri, address account) ERC1155(uri) Ownable(account) {}
+contract WavesERC1155Token is Initializable, ERC1155Upgradeable, OwnableUpgradeable {
+    uint256 public maxWavePerCreator = 20;
+    mapping(address => mapping(uint256 => uint256)) public tokensMintedByCreator;
+
+    function initialize(string memory _uri, address account, uint256 _maxWavePerCreator) public initializer {
+        __ERC1155_init(_uri);
+        __Ownable_init(account);
+        maxWavePerCreator = _maxWavePerCreator;
+    }
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {}
 
     function mint(address account, uint256 id, uint256 amount, bytes memory data) public onlyOwner {
+        // Check if the creator has reached the maximum number of waves
+        require(tokensMintedByCreator[account][id] + amount <= maxWavePerCreator, "Exceeds maximum mintable waves");
         _mint(account, id, amount, data);
+        tokensMintedByCreator[account][id] += amount;
     }
 
     function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) public onlyOwner {
         _mintBatch(to, ids, amounts, data);
+        for (uint256 i = 0; i < ids.length; i++) {
+            tokensMintedByCreator[to][ids[i]] += amounts[i];
+        }
+    }
+
+    function setMaxWavePerCreator(uint256 _maxWavePerCreator) external onlyOwner {
+        maxWavePerCreator = _maxWavePerCreator;
+    }
+
+    function uri(uint256 id) public view override returns (string memory) {
+        return string(abi.encodePacked(super.uri(id), ".json"));
+    }
+
+    function setURI(string memory newuri) external onlyOwner {
+        _setURI(newuri);
+    }
+
+    function burn(address account, uint256 id, uint256 amount) external onlyOwner {
+        _burn(account, id, amount);
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,11 +4,9 @@ async function main() {
 
   const WavesERC1155Token =
     await ethers.getContractFactory('WavesERC1155Token');
-  const wavesERC1155Token = await WavesERC1155Token.deploy(
-    'https://example.com/metadata/{id}.json',
-    'YOUR-ADDRESS-HERE',
-  );
+  const DEFAULT_WAVE_PER_CREATOR = 20;
 
+  const wavesERC1155Token = await upgrades.deployProxy(WavesERC1155Token, ["https://ipfs.io/ipfs/bafybeihjjkwdrxxjnuwevlqtqmh3iegcadc32sio4wmo7bv2gbf34qs34a/{id}.json", deployer.address, DEFAULT_WAVE_PER_CREATOR], { initializer: 'initialize' });
   console.log('WavesERC1155Token deployed to:', wavesERC1155Token.target);
 }
 
